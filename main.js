9b35b37e574a01842c280bd9a07e6e2928717526
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2018-10-18 19:54:56 -0300

    commit.

--- main.js
+++ main.js
@@ -5,14 +5,21 @@
  * Author: Glaucia Lemos
  */
 
-const { app, BrowserWindow } = require('electron');
+const electron = require('electron');
+const {
+  app,
+  BrowserWindow,
+} = require('electron');
 
 let win;
 
 function createWindow() {
-  win = new BrowserWindow({ width: 800, height: 600 });
+  win = new BrowserWindow({
+    width: 800,
+    height: 600,
+  });
 
-  win.loadFile('../index.html');
+  win.loadFile('./index.html');
 
   win.webContents.openDevTools();
 
@@ -24,15 +31,13 @@ function createWindow() {
 app.on('ready', createWindow);
 
 app.on('window-all-closed', () => {
-  // No macOS é comum para aplicativos e sua barra de menu
-  // permaneçam ativo até que o usuário explicitamente encerre com Cmd + Q
   if (process.platform !== 'darwin') {
     app.quit();
   }
 });
 
 app.on('active', () => {
-  if (win === null) {
+  if (win == null) {
     createWindow();
   }
 });
