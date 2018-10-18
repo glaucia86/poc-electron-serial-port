b4963b55ebe4320c077f66a5aaf2684a70e901ff
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2018-10-18 19:03:59 -0300

    Fim do desenvolvimento do Hello World em Electron

--- src/js/main.js
+++ src/js/main.js
@@ -4,3 +4,35 @@
  * Descrição: arquivo responsável por executar a aplicação Electron
  * Author: Glaucia Lemos
  */
+
+const { app, BrowserWindow } = require('electron');
+
+let win;
+
+function createWindow() {
+  win = new BrowserWindow({ width: 800, height: 600 });
+
+  win.loadFile('../index.html');
+
+  win.webContents.openDevTools();
+
+  win.on('closed', () => {
+    win = null;
+  });
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+  // No macOS é comum para aplicativos e sua barra de menu
+  // permaneçam ativo até que o usuário explicitamente encerre com Cmd + Q
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('active', () => {
+  if (win === null) {
+    createWindow();
+  }
+});
